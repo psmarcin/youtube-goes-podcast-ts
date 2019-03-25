f4c036bf6cca28e7e0e2091e7148803eef7bedc8
psmarcin <ps@psmarcin.me>
2019-03-25 15:50:24 +0100

    test: set expire date to future time

--- src/video/index.spec.ts
+++ src/video/index.spec.ts
@@ -25,7 +25,7 @@ describe("video", () => {
     it("should get redirection link", async () => {
       const link: string = await getRedirectLink(videoId);
       expect(link).toBe(
-        "https://r4---sn-f5f7lne6.googlevideo.com/videoplayback?gir=yes&clen=9665628&mn=sn-f5f7lne6%2Csn-4g5e6nze&mt=1553520323&mv=m&ei=b9eYXNy3Eo7u7gTciZ7wCg&ms=au%2Conr&requiressl=yes&mime=audio%2Fmp4&txp=5532432&pl=13&itag=140&mm=31%2C26&keepalive=yes&c=WEB&sparams=clen%2Cdur%2Cei%2Cgir%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Ckeepalive%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Crequiressl%2Csource%2Cexpire&ipbits=0&signature=19A7C990F0B708A32C92203B3EC41BF705CCDA67.3EB34CDFBBC25BDCE4C6FF55E2DD212331EFE9F6&key=yt6&ip=89.65.245.94&lmt=1550182498369207&dur=597.193&expire=1553542095&source=youtube&initcwndbps=1560000&fvip=4&id=o-APGyMx3Rq0zpvgIYYTaX0uP554VM2iVBDuxMZfM0E7QB&ratebypass=yes"
+        "https://r4---sn-f5f7lne6.googlevideo.com/videoplayback?clen=9665628&itag=140&txp=5532432&mime=audio%2Fmp4&gir=yes&key=yt6&lmt=1550182498369207&ipbits=0&dur=597.193&fvip=4&expire=1953547240&id=o-AASobwTXRER8wGgq6A3z3XTARJyMCj2XAvOUriK8zDLh&pl=13&signature=81545D8F4D7A5B05D8911D1CFA6F87C40F69477C.D10C2447C8F33ECC82A70205C433EAB9484CF5C1&mm=31%2C26&mn=sn-f5f7lne6%2Csn-4g5ednsk&ip=89.65.245.94&requiressl=yes&ms=au%2Conr&mt=1553525563&sparams=clen%2Cdur%2Cei%2Cgir%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Ckeepalive%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Crequiressl%2Csource%2Cexpire&mv=m&keepalive=yes&ei=iOuYXOzVC-Hk7ASKkI7oCA&c=WEB&initcwndbps=1473750&source=youtube&ratebypass=yes"
       );
     });
   });
