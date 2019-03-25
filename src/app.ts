37f400a15e659dfdd38093ed63b2c77d0700b1ff
psmarcin <ps@psmarcin.me>
2019-03-25 15:16:28 +0100

    test: integration setup

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import cors from "cors";
+import express, { Express } from "express";
+import config from "./config";
+import log from "./log";
+import logMiddleware from "./log/middleware";
+import { error404Handler, error5xxHandler } from "./middlewares/errors";
+import routes from "./routes";
+
+export const app: Express = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:8081",
+      "podcasts.psmarcin.me",
+      "https://yt.psmarcin.me"
+    ]
+  })
+);
+app.use(logMiddleware);
+app.use(routes);
+app.use(error404Handler);
+app.use(error5xxHandler);
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,11 @@
-import cors from "cors";
-import express, { Express } from "express";
+import { app } from "./app";
 import config from "./config";
 import log from "./log";
-import logMiddleware from "./log/middleware";
-import { error404Handler, error5xxHandler } from "./middlewares/errors";
-import routes from "./routes";
-
-const app: Express = express();
-
-app.use(
-  cors({
-    origin: [
-      "http://localhost:8081",
-      "podcasts.psmarcin.me",
-      "https://yt.psmarcin.me"
-    ]
-  })
-);
-app.use(logMiddleware);
-app.use(routes);
-app.use(error404Handler);
-app.use(error5xxHandler);
 
 function serverCallback(): void {
   log.info(`[SERVER] Listening on port: ${config.port}`);
 }
 
 app.listen(config.port, serverCallback);
+
+export const server = app;
