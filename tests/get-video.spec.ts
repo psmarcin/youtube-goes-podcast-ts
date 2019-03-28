aeb8de6d5cd2ef0b0194bd0cdf506bbf243cc577
psmarcin <ps@psmarcin.me>
2019-03-28 14:13:28 +0100

    fix: support old links without extension

--- tests/get-video.spec.ts
+++ tests/get-video.spec.ts
@@ -4,13 +4,20 @@ import { app } from "./../src/app";
 describe("GET /video", () => {
   it("should get redirect link", done => {
     supertest(app)
+      .get("/video/NtQkz0aRDe8.mp4")
+      .expect(302)
+      .end(done);
+  });
+  it("should get redirect link to old route", done => {
+    supertest(app)
       .get("/video/NtQkz0aRDe8")
       .expect(302)
       .end(done);
   });
+
   it("should get return nothing", done => {
     supertest(app)
-      .get("/video/NOT_VALID_VIDEO_ID")
+      .get("/video/NOT_VALID_VIDEO_ID.mp4")
       .expect(400)
       .end(done);
   });
